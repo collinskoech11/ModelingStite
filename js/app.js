55b1823e05a2730a007dbf97ac8dc74c9d534f90
collinskoech11 <collinskoechck34@gmail.com>
2020-07-14 13:31:38 +0300

    .

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 document.getElementById('button').addEventListener('click',
 function() {
 	document.querySelector('.bg-modal').style.display = 'flex';
-});
 
+});
 document.querySelector('.close').addEventListener('click',
 function() {
 	document.querySelector('.bg-modal').style.display = 'none';
+});
+
+
+document.getElementById('button1'),addEventListener('click',
+function(){
+	document.querySelector('.bg-modal-1').style.display = 'flex';
+});
+document.querySelector('.close-1').addEventListener('click',
+function() {
+	document.querySelector('.bg-modal-1').style.display = 'none';
 });

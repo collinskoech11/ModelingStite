f57cf57d72e63ee74378dcce8cd5dd9e7eda01a1
collinskoech11 <collinskoechck34@gmail.com>
2020-07-18 21:34:20 +0300

    hosted on firebase

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+document.getElementById('button').addEventListener('click',
+function() {
+	document.querySelector('.bg-modal').style.display = 'flex';
+
+});
+document.querySelector('.close').addEventListener('click',
+function() {
+	document.querySelector('.bg-modal').style.display = 'none';
+});
+
